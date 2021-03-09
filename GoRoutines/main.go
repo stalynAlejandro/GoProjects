02a24fcaca18e255de118131a1d9df4332ec7f58
stalynAlejandro <saavalencia97@gmail.com>
2021-03-09 02:13:35 +0100

    go routines

---
+++ GoRoutines/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
+
+func main() {
+
+	fmt.Printf("Thread: %v\n", runtime.GOMAXPROCS(-1))
+
+	runtime.GOMAXPROCS(100)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Printf("Hello #%v\n", counter)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	m.Unlock()
+	counter++
+	wg.Done()
+}
